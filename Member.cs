1fd53de212ee14ffb68d0243f4f87f534b65c08d
moh-bash <bashirkuja@gmail.com>
2025-10-20 10:51:52 +0300

    fix

--- Member.cs
+++ Member.cs
@@ -1,14 +1,1 @@
-﻿public class Member : Person
-{
-    public string Id { get; set; }
-
-    public Member(string id, string name, string email) : base(name, email)
-    {
-        Id = id;
-    }
-
-    public override string ToString()
-    {
-        return $"Member: {Name}, Email: {Email}, ID: {Id}";
-    }
-}
+﻿
